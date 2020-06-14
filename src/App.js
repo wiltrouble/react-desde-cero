22ff3140a8430ec4a2d03d65f972fcd4549ee617
Wilson Lopez <wiltrouble@gmail.com>
2020-06-14 12:14:20 -0400

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import "./styles/styles.scss";
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+  
+<div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" src="https://edteam-media.s3.amazonaws.com/courses/original/bcb63ac0-5dc5-48b7-a111-9974a7fc3bcd.png"></img>
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Título del banner</p>
+        <p> Subtítulo del banner</p>
+        <a href="#" class="button">Suscribe</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+
+<div className="ed-grid m-grid-3">
+  <Curso></Curso>
+  <Curso></Curso>
+  <Curso></Curso>
+  <Curso></Curso>
+  <Curso></Curso>
+
+  <Curso></Curso>
+  <Curso></Curso>
+  <Curso></Curso>
+  <Curso></Curso>
+  <Curso></Curso>
+</div>
+  </>
+)
 
 export default App;
+
+// Reglas JSX
+// Los componenetes deben cerrarce
+// Los componentes deben devolver un solo elemento padre
+// apoyarse de los fragments cuando necesito devolver 2 elementos
+//  Fragmanet o <></>
---
+++ src/Curso.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Curso = () => (
+    <article className="card">
+  <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+    <img src="https://edteam-media.s3.amazonaws.com/courses/original/e8207ec4-e9a7-4cba-86a8-ee80c443a37f.jpg" alt="poster del curso"/>
+  </div>
+  <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+    <h3 className="t5 s-mb-2 s-center">
+      Programación orientada a objetos con Go
+    </h3>
+    <div className="s-mb-2 s-main-center">
+      <div className="card__teacher s-cross-center">
+        <div className="card__avatar s-mr-1">
+          <div className="circle img-container">
+            <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/e19b1e7f-6dcc-495b-883c-7d0b9b3fdf15.jpg" alt="tio alexis"/>
+          </div>
+        </div>
+        <span className="small">Alexys Lozada</span>
+      </div>
+    </div>
+    <div className="s-main-center">
+      <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+    </div>
+  </div>
+</article>
+)
+
+export default Curso;
